c6e01d2c86c71b7f3b5855be163e945ddaa5c0ce
Juraj <juraj.marusiak@gmail.com>
2016-07-28 06:22:19 +0200

    Fixed home route ..

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,10 @@ Route::group(['prefix' => 'administrator'], function () {
     Route::resource('category', 'Admin\CategoryController');
     Route::get('/media', 'Admin\MediaController@index');
     Route::get('/media/embedded', 'Admin\MediaController@embedded');
-//    Route::get('/', [
-//        'middleware' => 'auth',
-//        'uses' => 'Admin\HomeController@index'
-//    ]);
+    Route::get('/', [
+        'middleware' => 'auth',
+        'uses' => 'Admin\ArticleController@index'
+    ]);
     Route::auth();
     Route::get('/home', 'Admin\HomeController@index');
 });

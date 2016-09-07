75419a95c3bef0f9f8c5352422aa2f0446dc87ce
Juraj <juraj.marusiak@gmail.com>
2016-09-07 21:13:36 +0200

    Fabric changes ..

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,18 @@
 from __future__ import with_statement
 from fabric.api import *
+import ConfigParser
 
-env.hosts = ["shellserver.websupport.sk:12304"]
-env.user = "uid58820"
-env.password = "a0598dd493"
-deploy_dir = "/home/yuma.sk/sub/test"
+#########################################################
+# Config
+#########################################################
+
+def set_config(e):
+  config = ConfigParser.ConfigParser()
+  config.read('./fabfile.conf')
+  env.host_string = config.get(e,'host') +':'+ config.get(e,'port')
+  env.user = config.get(e,'user')
+  env.password = config.get(e,'password')
+  env.deploy_dir = config.get(e,'deploy_dir')
 
 def commit():
   local("git add -p && git commit")
@@ -12,13 +20,16 @@ def commit():
 def push():
   local("git push")
 
+@task
 def prepare_deploy():
   commit()
   push()
 
-def deploy():
+@task
+def deploy(e="testing"):
+  set_config(e);
   with settings(warn_only=True):
-    run("git clone git@bitbucket.org:durino13/cms.git %s" % deploy_dir)
-  with cd(deploy_dir):
+    run("git clone git@bitbucket.org:durino13/cms.git %s" % env.deploy_dir)
+  with cd(env.deploy_dir):
     run("git pull")
     run("composer install")
